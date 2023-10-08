14f7cadb93ce10b1fa7a668bf5a5624b57b725de
iamadityaanjana <76644021+iamadityaanjana@users.noreply.github.com>
2023-10-08 22:25:44 +0530

    Update 3_🌏_Learn_More.py

--- pages/3_🌏_Learn_More.py
+++ pages/3_🌏_Learn_More.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
+from PIL import Image 
 
+#configuring page layout
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 st.markdown(
     """
@@ -15,8 +16,11 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+#title for web page
 st.title('Know more about Carbon Emissions and footprint')
 
+
+#subheadings and text for all the questions answered
 st.subheader('What exactly is Carbon Footprint?')
 
 st.write("A carbon footprint is a quantification of the total volume of greenhouse gases, primarily carbon dioxide (CO2), and other carbon compounds released directly or indirectly as a result of the actions of an individual, a company, an event, or a product during a specific timeframe. It is usually expressed in units of carbon dioxide equivalents (CO2e), which take into consideration the varying climate-warming potentials of different greenhouse gases.")
@@ -27,4 +31,5 @@ st.write("The harmful effects of CO2 emissions can be seen in several ways:")
 st.write("Carbon dioxide (CO2) emissions are highly detrimental due to their central role in exacerbating global warming and climate change. CO2, as a greenhouse gas, intensifies the greenhouse effect by trapping heat within the Earth's atmosphere. This, in turn, leads to a range of devastating consequences, including the rise in global temperatures, which fosters more frequent and severe heatwaves, droughts, and extreme weather events, causing havoc for ecosystems, agriculture, and human societies alike. Furthermore, rising temperatures result in the melting of polar ice caps and glaciers, contributing to sea-level rise that triggers coastal erosion, heightened flooding, and the displacement of vulnerable coastal communities.")
 
 st.subheader("How to reduce Carbon Emissions?")
+#initializing image input from external source and citing it in captions
 st.image("https://sustainability.georgetown.edu/wp-content/uploads/sites/28/2018/12/top10_final.jpg", caption="Source: https://sustainability.georgetown.edu")
